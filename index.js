d2382e204554597e4515a41d9256f3868e0f277d
Fredy Betancur <fabetancc@gmail.com>
2022-01-18 18:55:48 -0500

    Se finaliza la aplicaci{on de consola del clima de las ciudades.

--- index.js
+++ index.js
@@ -25,24 +25,38 @@ const main = async() => {
                 lugares = await busquedas.ciudad(termino);
 
                 const id = await listarLugares(lugares);
+                if(id === 0) continue;
+
                 const lugarSel = lugares.find(l => l.id === id);
 
+                // Guardar en DB
+                busquedas.agregarHistorial(lugarSel.nombre);
+
+                // Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
+
+                console.clear();
                 console.log('\nInformación de la ciudad\n'.green);
-                console.log('Ciudad:', lugarSel.nombre);
-                console.log('Lat:', );
-                console.log('Lng:', );
-                console.log('Temperatura:', );
-                console.log('Mínima:', );
-                console.log('Máxima:', );
+                console.log('Ciudad:', lugarSel.nombre.green);
+                console.log('Lat:', lugarSel.lat);
+                console.log('Lng:', lugarSel.lng);
+                console.log('Temperatura:', clima.temp);
+                console.log('Mínima:', clima.min);
+                console.log('Máxima:', clima.max);
+                console.log('Cómo está el clima:', clima.desc.green);
             break;
 
             case 2:
                 // Historial
-                console.log('Historial');
+                busquedas.historialCapitalizado.forEach((lugar, i) => {
+                    const idx = `${i+1}.`.green;
+                    console.log(idx, lugar);
+                });
             break;
             
             case 0:
                 // Salir
+                console.clear();
                 process.exit();
             break;
         
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,13 +1,23 @@
-const axios = require('axios')
+const fs = require('fs');
+const axios = require('axios');
 
 class Busquedas {
 
     historial = [];
+    dbPath = './db/database.json';
 
     constructor(){
 
-        // Leer DB si existe.
+        this.leerDB();
 
+    }
+
+    get historialCapitalizado(){
+
+        return this.historial.map(lugar => {
+            return lugar.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
+        });
+       
     }
 
     get paramsMapbox(){
@@ -18,6 +28,14 @@ class Busquedas {
         };
     }
 
+    get paramsOpenweather(){
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
+        };
+    }
+ 
     async ciudad(lugar = ''){
 
         try {
@@ -41,6 +59,60 @@ class Busquedas {
         }
 
 
+    }
+
+    async climaLugar(lat, lon){
+
+        try {
+            
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: {...this.paramsOpenweather, lat, lon}
+            });
+
+            const resp = await instance.get();
+            const {weather, main} = resp.data;
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    agregarHistorial(lugar = ''){
+
+        if (this.historial.includes(lugar.toLowerCase())) {
+            return;
+        }
+
+        this.historial = this.historial.splice(0, 4);
+
+        this.historial.unshift(lugar.toLowerCase());
+        this.guardarDB();
+
+    }
+
+    guardarDB(){
+
+        const payload = {
+            historial: this.historial
+        };
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB(){
+        if (!fs.existsSync(this.dbPath)) return null;
+    
+        const info = fs.readFileSync(this.dbPath, {encoding: 'utf-8'});
+        const data = JSON.parse(info);
+        this.historial = data.historial;
     }
 
 }
